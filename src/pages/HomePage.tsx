b3c104ccaf7c84f58d717f284e5f8db58423d520
Nagib <nagibsaidhg2005@gmail.com>
2025-03-09 18:17:40 -0500

    logo de la pagina

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,40 +1,39 @@
-import { Brands } from '../components/home/Brands';
-import { FeatureGrid } from '../components/home/FeatureGrid';
-import { ProductGrid } from '../components/home/ProductGrid';
-import { ProductGridSkeleton } from '../components/skeletons/ProductGridSkeleton';
-import { prepareProducts } from '../helpers';
-import { useHomeProducts } from '../hooks';
+import { Brands } from "../components/home/Brands";
+import { FeatureGrid } from "../components/home/FeatureGrid";
+import { ProductGrid } from "../components/home/ProductGrid";
+import { ProductGridSkeleton } from "../components/skeletons/ProductGridSkeleton";
+import { prepareProducts } from "../helpers";
+import { useHomeProducts } from "../hooks";
 
 export const HomePage = () => {
-	const { recentProducts, popularProducts, isLoading } =
-		useHomeProducts();
+  const { recentProducts, popularProducts, isLoading } = useHomeProducts();
 
-	const preparedRecentProducts = prepareProducts(recentProducts);
-	const preparedPopularProducts = prepareProducts(popularProducts);
+  const preparedRecentProducts = prepareProducts(recentProducts);
+  const preparedPopularProducts = prepareProducts(popularProducts);
 
-	return (
-		<div>
-			<FeatureGrid />
+  return (
+    <div>
+      <FeatureGrid />
 
-			{isLoading ? (
-				<ProductGridSkeleton numberOfProducts={4} />
-			) : (
-				<ProductGrid
-					title='Nuevos Productos'
-					products={preparedRecentProducts}
-				/>
-			)}
+      {isLoading ? (
+        <ProductGridSkeleton numberOfProducts={4} />
+      ) : (
+        <ProductGrid
+          title="Nuevos Productos"
+          products={preparedRecentProducts}
+        />
+      )}
 
-			{isLoading ? (
-				<ProductGridSkeleton numberOfProducts={4} />
-			) : (
-				<ProductGrid
-					title='Productos Destacados'
-					products={preparedPopularProducts}
-				/>
-			)}
+      {isLoading ? (
+        <ProductGridSkeleton numberOfProducts={4} />
+      ) : (
+        <ProductGrid
+          title="Productos Destacados"
+          products={preparedPopularProducts}
+        />
+      )}
 
-			<Brands />
-		</div>
-	);
+      <Brands />
+    </div>
+  );
 };
